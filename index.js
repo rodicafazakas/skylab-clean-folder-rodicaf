a258e1245016ef74810425228687b607fe3ab4e1
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-12 13:43:43 +0100

    Changed function

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ exports.greet = (name)=>{
     console.log(`Hi ${name}`);
 }
 
+const fs = require("fs");
 
+fs.readdir("./DeleteMe", async (error, files)=>{
+    if(error) throw error;
+
+    for(let key in files) {
+        await fs.unlink('./DeleteMe/' + files[key], (error) => { 
+          if (error) throw error;
+          console.log('File deleted: ' + files[key]);
+        })
+    }
+});   
 
